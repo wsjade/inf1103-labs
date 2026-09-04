75bf67aa6b8a51d1ab3e09b1d2d78a179ae64ba9
tianyu <2605688@sit.singaporetech.edu.sg>
2026-09-04 17:00:55 +0800

    Forth Post

--- hello.py
+++ hello.py
@@ -3,19 +3,29 @@ print("Welcome here")
 print("My first post!")
 print("=============================")
 
-username = "cool_creater"
-bio = "Fun Blogger"
-followers = 100
+# username = "cool_creater"
+# bio = "Fun Blogger"
+# followers = 100
 
+# print("Username:", username)
+# print("Bio:", bio)
+# print("Followers:", followers)
+
+# followers += 50
+# print("Day 1:", followers)
+
+# followers += 20
+# print("Day 2:", followers)
+
+# followers -= 10   
+# print("Day 1:", followers)
+
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("====================")
 print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
-
-followers += 50
-print("Day 1:", followers)
-
-followers += 20
-print("Day 2:", followers)
-
-followers -= 10   
-print("Day 1:", followers)
+print("Age:", age)
+print("Category:", category)
