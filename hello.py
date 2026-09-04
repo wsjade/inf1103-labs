727efa87c1deb611a0e86b9389a3f9882738899d
tianyu <2605688@sit.singaporetech.edu.sg>
2026-09-04 17:03:20 +0800

    Fifth post

--- hello.py
+++ hello.py
@@ -21,7 +21,7 @@ print("=============================")
 # print("Day 1:", followers)
 
 username = input("Enter Username: ")
-age = input("Enter Age: ")
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
@@ -29,3 +29,6 @@ print("====================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
