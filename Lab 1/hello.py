62da0a41635845ba2f32536aba27d52e83bc1b5f
tianyu <2605688@sit.singaporetech.edu.sg>
2026-09-11 16:52:25 +0800

    Organized Lab 1 files

--- Lab 1/hello.py
+++ Lab 1/hello.py
@@ -30,5 +30,5 @@ print("Username:", username)
 print("Age:", age)
 print("Category:", category)
 
-if age>40 and category == "fun":
+if age > 40 and category == "fun":
     print("You are old what is fun for you??")
