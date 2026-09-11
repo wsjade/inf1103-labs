15464c3c3f31b7bb97f7e89b8d8e788b8815ac68
tianyu <2605688@sit.singaporetech.edu.sg>
2026-09-11 17:22:50 +0800

    Added handling invalid input

--- Lab 2/auditor.py
+++ Lab 2/auditor.py
@@ -6,4 +6,9 @@ while True:
     if stock == "quit":
         break
 
+    if not stock.isdigit():
+        print("Error: Please enter a valid number.")
+        continue
+
+    stock = int(stock)
     print(stock)
