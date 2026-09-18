e10cba5bfbbb1daa78028bfa1c4542534e53c724
tianyu <2605688@sit.singaporetech.edu.sg>
2026-09-18 20:48:51 +0800

    Initialised inventory

---
+++ Lab 3/modular_auditor.py
@@ -0,0 +1,31 @@
+inventory = 0
+failed_entries = 0
+
+while True:
+    stock = input("Enter stock quantity or 'quit': ")
+
+    if stock == "quit":
+        break
+
+    if not stock.isdigit():
+        print("Error: Please enter a valid number.")
+        failed_entries = failed_entries + 11
+        continue
+
+    stock = int(stock)
+
+    if stock < 0:
+        print("Error: Stock quantity cannot be negative.")
+        failed_entries = failed_entries + 1
+        continue
+
+    inventory = inventory + stock
+
+    print("Current inventory:", inventory)
+
+    if inventory > 500:
+        print("ALERT: Overstock limit exceeded! Please keep it at 500.")
+        break
+
+print("Total Units Processed:", inventory)
+print("Number of Failed/Rejected Entries:", failed_entries)
